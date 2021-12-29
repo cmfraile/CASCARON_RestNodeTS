ade93d24c58a8d4421353d3dd7b2f45dfdab8334
Carlos Fraile <carlosfrailegomez93@gmail.com>
2021-12-29 17:15:50 +0100

    203

--- dist/models/server.js
+++ dist/models/server.js
@@ -4,14 +4,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var http_1 = require("http");
+var cors = require('cors');
 var Server = /** @class */ (function () {
     function Server() {
         this.app = (0, express_1.default)();
         this.port = process.env.PORT || '8000';
+        this.httpserver = (0, http_1.createServer)();
+        this.io = require('socket.io')(this.httpserver);
+        this.paths = { ejemplo: '/api/ejemplo' };
+        this.middlewares();
+        //this.routes();
     }
+    Server.prototype.middlewares = function () {
+        this.app.use(cors());
+        this.app.options('*', cors());
+    };
+    Server.prototype.routes = function () {
+        this.app.use(this.paths.ejemplo, require('../routes/ejemplo'));
+    };
     Server.prototype.listen = function () {
         var _this = this;
-        this.app.listen(this.port, function () {
+        this.httpserver.listen(this.port, function () {
             console.log("Servidor corriendo en puerto ".concat(_this.port));
             console.log("tsc --watch && nodemon dist/app.js &&");
         });
--- models/server.ts
+++ models/server.ts
@@ -1,4 +1,5 @@
 import express , { Application } from 'express';
+import { createServer,Server as httpserver } from 'http';
 const cors = require('cors');
 
 class Server {
@@ -6,10 +7,14 @@ class Server {
     private app:Application;
     private port:string;
     private paths:any;
+    private httpserver:httpserver;
+    private io;
 
     constructor(){
         this.app = express();
         this.port = process.env.PORT || '8000';
+        this.httpserver = createServer();
+        this.io = require('socket.io')(this.httpserver);
         this.paths = { ejemplo:'/api/ejemplo' }
         this.middlewares();
         //this.routes();
@@ -24,7 +29,7 @@ class Server {
     }
 
     listen(){
-        this.app.listen(this.port, () => {
+        this.httpserver.listen(this.port, () => {
             console.log(`Servidor corriendo en puerto ${this.port}`);
             console.log("tsc --watch && nodemon dist/app.js &&");
         })
